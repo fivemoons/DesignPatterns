993ee43bb5e6074bb48dd37a5232fbbd43a7b113
fivemoons <liuyuyao1992@qq.com>
2015-10-09 19:43:40 +0800

    New Singleton

---
+++ src/No_02Factory/Factory.java
@@ -0,0 +1,12 @@
+package No_02Factory;
+
+import java.math.BigInteger;
+
+
+public class Factory
+{
+	public static void main(String[] args) {
+		System.out.println(Boolean.class);
+		BigInteger.probablePrime(bitLength, rnd);
+	}
+}
